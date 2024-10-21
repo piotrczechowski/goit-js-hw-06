9b3964104e950617a492110b5c975124189b8c81
piotrczechowski <58622514+piotrczechowski@users.noreply.github.com>
2024-10-21 21:45:24 +0200

    task 1 and 2

---
+++ js/task-2.js
@@ -0,0 +1,32 @@
+class Storage {
+    #items;
+
+
+constructor(items) {
+    this.#items = items;
+}
+
+getItems() {
+    return this.#items;
+}
+
+addItem(newItem){
+    this.#items.push(newItem);
+}
+
+removeItem(itemToRemove) {
+    const itemIndex = this.#items.indexOf(itemToRemove);
+    if (itemIndex !== -1) {
+        this.#items.splice(itemIndex, 1);
+    }
+}
+}
+
+
+// Check
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
